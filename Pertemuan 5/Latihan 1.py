696b2405b5ca56700ff60cc37e2084aceecaf79f
zedazedi <aziizaca14@gmail.com>
2025-03-19 18:59:56 +0700

    Update Latihan 1.py

--- Pertemuan 5/Latihan 1.py
+++ Pertemuan 5/Latihan 1.py
@@ -1,1 +1,20 @@
+class Yunani:
+    def ibukota(self):
+        print("Ibukota Yunani adalah Athena")
 
+    def mata_uang(self):
+        print("Mata uang Yunani adalah Euro (EUR)")
+
+class Korea_selatan:
+    def ibukota(self):
+        print("Ibukota Korea Selatan adalah Seoul")
+
+    def mata_uang(self):
+        print("Mata uang Korea Selatan adalah Won Korea Selatan (KRW)")
+
+yunani = Yunani()
+korsel = Korea_selatan()
+
+for negara in (yunani, korsel):
+    negara.ibukota()
+    negara.mata_uang()
